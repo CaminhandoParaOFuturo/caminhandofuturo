15c286b4e805e7cc9af326596bcb16ebffaaa5bc
Adrianodisam <adrianodisam@gmail.com>
2022-09-03 01:27:12 -0300

    responsivo home

--- src/view/components/Home/Header/Header.tsx
+++ src/view/components/Home/Header/Header.tsx
@@ -1,61 +1,58 @@
 import React from 'react';
-import BtnScrollHeader from '../BtnScrollHeader/BtnScrollHeader';
 import Link from 'next/link'
 import styles from './Header.module.css';
+import useMedia from '../../../../Hooks/useMedia';
 
 
 const Header = () => {
+
+  const mobile = useMedia('(max-width: 40rem)');
+  const [ativo, setAtivo] = React.useState(false);
+  const ToogleMode = () => {
+    setAtivo(!ativo);
+  };
+
   return (
     <>
     <div className={styles.header}>
       <div className={styles.divMenu}>
-        <img src={'/images/Artboard 1 1.png'} alt="logo" />
-        <div className={styles.menu}>
-          <ul>
+        <img src={'/images/Artboard 1 1.png'} alt="logo" className={styles.imgMobile} />
+        {mobile && (
+        <button
+          className={`${styles.mobileButton} ${
+            ativo && styles.mobileButtonAtivo
+          }`}
+          aria-label="Menu"
+          onClick={ToogleMode}
+        ></button>
+      )}
+
+ <div className={ styles.menu}>
+            <ul >
+              <li>
+                <Link href="/home">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/projects">
+                  <a>Nossos Projetos</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/nucleus">
+                  <a>Nossos Nucleos</a>
+                </Link>
+              </li>
             <li>
-              <Link href="/home">
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/projects">
-                <a>Nossos Projetos</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/nucleus">
-                <a>Nossos Nucleos</a>
-              </Link>
-            </li>
-            <li>
-               <Link href="/sobre">
-                <a>Sobre Nós</a>
-              </Link>
-            </li>
-           <li>
-              <Link href="/Ajude">
-                    <a>Ajude</a>
-              </Link>
-            </li>
-            {/*  <li>Parceiros</li>
-            <li>Contatos</li> */}
-          </ul>
-        </div>
+                <Link href="/Ajude">
+                      <a>Ajude</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div> 
       </div>
-
-      {/* <div className={styles.divTextosContainer}>
-        <div className={styles.divTextos}>
-          <div className={styles.divTextoP}>
-            <p>Transformando através da educação</p>
-          </div>
-          <div className={styles.botao}>
-            <h1>faça sua doação</h1>
-          </div>
-        </div>
-       <BtnScrollHeader/>
-      </div> */}
-    </div>
-
     </>
   );
 };
--- src/view/components/Home/Sobre/Sobre.js
+++ src/view/components/Home/Sobre/Sobre.js
@@ -1,53 +1,81 @@
 import React from 'react';
 import styles from './Sobre.module.css';
-
 import CardsSobre from './CardsSobre';
 import { Grid } from '@mui/material';
 import Link from 'next/link';
+import useMedia from '../../../../Hooks/useMedia';
 
 const Sobre = () => {
+  const mobile = useMedia('(max-width: 60rem)');
 
   return (
     <div className={styles.sobre}>
-      <div>
+      <div className={mobile ? styles.divSobreMobile : styles.divSobreN}>
         <div className={styles.divSobre}>
           <p className={styles.nosSobre}>SOBRE NÓS</p>
           <p className={styles.instSobre}>Conheça a instituição</p>
         </div>
-        <div className={styles.divSobreCard}>
-          <Link href="/projects"><a>
-            <CardsSobre
-              img="/images/003-building 1.png"
-              texto="nossos projetos"
-              cores={styles.CardAjude}
-            />
-          </a></Link>
-          <Link href="#quemSomos"><a>
-            <CardsSobre
-              img="/images/001-user 1.png"
-              texto="quem somos"
-              cores={styles.cardProjetos}
-            />
-          </a></Link>
-          <Link href="/projects"><a>
-            <CardsSobre
-              img="/images/002-heart 3.png"
-              texto="ajude a instituição"
-              cores={styles.cardQuemSomos}
-            />
-          </a></Link>
+        <div
+          className={`${styles.divSobreCard} ${
+            mobile && styles.divSobreCardMob
+          }`}
+        >
+          <Link href="/projects">
+            <a>
+              <CardsSobre
+                img="/images/003-building 1.png"
+                texto="nossos projetos"
+                cores={styles.CardAjude}
+              />
+            </a>
+          </Link>
+          <Link href="#quemSomos">
+            <a>
+              <CardsSobre
+                img="/images/001-user 1.png"
+                texto="quem somos"
+                cores={styles.cardProjetos}
+              />
+            </a>
+          </Link>
+          <Link href="/projects">
+            <a>
+              <CardsSobre
+                img="/images/002-heart 3.png"
+                texto="ajude a instituição"
+                cores={styles.cardQuemSomos}
+              />
+            </a>
+          </Link>
         </div>
       </div>
-      <div className={styles.resumeDiv} id="quemSomos">
-        <div className={styles.resumeInfos}>
-          <Grid container spacing={1}>
-            <Grid item >
-              <p className={styles.resumeTitle}>Desde 1975 atuando na área da infância e juventude</p>
-              <p className={styles.resumeText}>Sociedade Beneficente Caminhando Para o Futuro, inciou suas atividades no ano de 1975, no dia 11 de julho. A inciativa surgiu de movimentos de moradores de bairro do Jd. Carombé para abertura de um núcleo que tivesse a proposta de atender à crianças e adolescentes de famílias que residem na região e assim à famílias poderiam ficar mais tranquilas, pois seus filhos estariam em um local seguro</p>
+      <div
+        className={`${styles.resumeDiv} ${mobile && styles.resumeDivMObile}`}
+        id="quemSomos"
+      >
+        <div className={`${styles.resumeInfos} ${mobile && styles.s}`}>
+          <Grid container spacing={1} className={mobile ? styles.Grid : ''}>
+            <Grid item>
+              <p className={styles.resumeTitle}>
+                Desde 1975 atuando na área da infância e juventude
+              </p>
+              <p className={styles.resumeText}>
+                Sociedade Beneficente Caminhando Para o Futuro, inciou suas
+                atividades no ano de 1975, no dia 11 de julho. A inciativa
+                surgiu de movimentos de moradores de bairro do Jd. Carombé para
+                abertura de um núcleo que tivesse a proposta de atender à
+                crianças e adolescentes de famílias que residem na região e
+                assim à famílias poderiam ficar mais tranquilas, pois seus
+                filhos estariam em um local seguro
+              </p>
             </Grid>
+
             <Grid item className={styles.resumeImageParent}>
               <div className={styles.resumeImageBackground1}></div>
-              <img className={styles.resumeImage} src="/images/3criancas.png"></img>
+              <img
+                className={styles.resumeImage}
+                src="/images/3criancas.png"
+              ></img>
               <div className={styles.resumeImageBackground2}></div>
             </Grid>
           </Grid>
