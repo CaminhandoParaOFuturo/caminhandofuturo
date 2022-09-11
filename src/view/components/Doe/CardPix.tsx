6928a585a8d61411fe8a64c5b6bee6cbfe677912
Adrianodisam <adrianodisam@gmail.com>
2022-09-11 15:29:36 -0300

    botçao copiar pix

--- src/view/components/Doe/CardPix.tsx
+++ src/view/components/Doe/CardPix.tsx
@@ -8,6 +8,15 @@ import Textopix from './Textopix';
 const CardPix = () => {
   const iputPix = useRef(null);
   const mobile = useMedia('(max-width: 60rem)');
+  function copyToClipBoard() {
+
+    var content = document.getElementById('input');
+    
+    content?.onselect;
+    document.execCommand('copy');
+
+    alert("Copied!");
+}
 
   return (
     <div className={ mobile ?styles.cardPixMobile : styles.cardPix }>
@@ -18,8 +27,11 @@ const CardPix = () => {
             <Textopix texto ="Abra o aplicativo do seu banco e escolha a opção PIX Copia e Cola" numero='2'/>
             <Textopix texto ="Confirma os dados da Caminhando para o Futuro e pronto" numero='3'/>
           </div>
-                     
-           <input type="text" ref={iputPix} className={styles.input} value="00020126360014BR.GOV.BCB.PIX0114473897880001335204000053039865802BR5924Caminhando Para o Futuro6009Sao Paulo62070503***6304A7AD" />
+            <div className={styles.DivCopiarpix}>   
+               <input type="text" ref={iputPix} className={styles.input} value="00020126360014BR.GOV.BCB.PIX0114473897880001335204000053039865802BR5924Caminhando Para o Futuro6009Sao Paulo62070503***6304A7AD" />
+               <button onClick={copyToClipBoard}>Copiar</button>
+             </div>        
+       
         </div>
         <div className={styles.cardI}>
         <strong>Ou</strong>
